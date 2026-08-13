9dcf27f4d04def5d35e9689cf08ac50013e5820c
thiyagumitrahsoft-tech <thiyagumitrahsoft@gmail.com>
2026-08-13 19:56:50 +0530

    bug fix

---
+++ src/main/java/com/example/orderservice/dto/InventoryResponse.java
@@ -0,0 +1,17 @@
+package com.example.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryResponse {
+
+    private Long productId;
+
+    private Integer availableQuantity;
+}
