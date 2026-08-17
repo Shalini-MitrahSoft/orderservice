1f89e115996429a3b974d51961a3a099da527106
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 19:26:42 +0530

    Implemented a swagger

--- src/main/java/com/example/orderservice/OrderserviceApplication.java
+++ src/main/java/com/example/orderservice/OrderserviceApplication.java
@@ -2,6 +2,7 @@ package com.example.orderservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 		title = "Order Service API",
 		version = "v1",
 		description = "Order management APIs for the Order Management System"
-))
+), servers = @Server(url = "/", description = "Current host"))
 @EnableFeignClients(
 		basePackages = "com.example.orderservice.client"
 )
