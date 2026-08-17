bcc9e6e3a6e2313a5f9b0c4c9a54991ec295367b
Shalini <shalini.m@mitrahsoft.in>
2026-08-17 16:12:24 +0530

    Implemented a actuator and swagger

--- src/main/java/com/example/orderservice/OrderserviceApplication.java
+++ src/main/java/com/example/orderservice/OrderserviceApplication.java
@@ -1,11 +1,17 @@
 package com.example.orderservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Order Service API",
+		version = "v1",
+		description = "Order management APIs for the Order Management System"
+))
 @EnableFeignClients(
 		basePackages = "com.example.orderservice.client"
 )
